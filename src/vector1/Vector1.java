b191ac920850dcd698a3c69639647a264988e2a5
Ismael Rivas H <ismaelrh1807@gmail.com>
2019-11-30 14:33:12 -0500

    1er Cambio

--- src/vector1/Vector1.java
+++ src/vector1/Vector1.java
@@ -15,7 +15,7 @@ public class Vector1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Vales verga princito por Dios santo");
+        System.out.println("He realizado un cambio");
     }
     
 }
