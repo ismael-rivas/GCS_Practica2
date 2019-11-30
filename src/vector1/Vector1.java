aa50a8737e970b165c2f7b5215b7de9e5728a497
Ismael Rivas H <ismaelrh1807@gmail.com>
2019-11-30 16:12:49 -0500

    Vector creado

--- src/vector1/Vector1.java
+++ src/vector1/Vector1.java
@@ -5,9 +5,13 @@
  */
 package vector1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Ismael Rivas H
+ * Gestión de la Configuración del Software
+ * 
  */
 public class Vector1 {
 
@@ -15,7 +19,31 @@ public class Vector1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("He realizado un cambio");
+        int [] vector = new int[10];
+        
+        Scanner reader = new Scanner(System.in);
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Ingrese el número ["+i+"] a continuación: ");
+            vector[i] = reader.nextInt();
+        }
+        System.out.println("\nVector ingresado:\n");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(vector[i]+"\t");
+        }
+        int aux = 0;
+        for (int i = 1; i < 10; i++) {
+            for (int j = 0; j < 9; j++) {
+                if(vector[j]>vector[j+1]){
+                     aux = vector[j+1];
+                     vector[j+1] = vector[j];
+                     vector[j] = aux;
+                }
+            }
+        }
+        System.out.println("\nVector ordenado:\n");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(vector[i]+"\t");
+        }
     }
-    
 }
